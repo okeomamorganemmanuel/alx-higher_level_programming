51a49b2bf883653a1fa59a0db410633997976dad
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-01-23 22:07:42 +0100

    add safe print file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    For i in range(x):
+        try
+            print(""format(my_list=[]) end="")
+            count += 1
+        except IndexError:
+            break
+    return count
