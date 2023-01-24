6ea4929c0527108189d5324fb358eda236ee291c
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-01-24 11:40:17 +0100

    add integers division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, NameError):
-        return None
+    except ZeroDivisionError:
+        result = 0
     finally:
-        print("inside result: None")
+        print("inside result:{}".format(result))
     return result
 
