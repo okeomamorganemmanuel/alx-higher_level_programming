a8973e4014f111ba0ac0b9c8fe9d21a8e21dc5bc
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-01-25 00:55:02 +0100

    add tassk 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,12 +2,16 @@
 
 """define a class square"""
 
+
 class Square:
     """A square"""
 
     def __init__(self, size):
         """initialize a new square
+
+
         Args:
             size (int): size of square
         """
+
         self.__size = size
