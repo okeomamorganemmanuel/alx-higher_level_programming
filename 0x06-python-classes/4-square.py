0e363fedd63ebf2f67b902c96d8b2bcf8b6c4854
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-01-25 01:36:15 +0100

    add tassk 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""a class that creates a square"""
+
+
+class Square:
+    """the square class"""
+
+    def __init__(self, size=0):
+        """initialize a new square
+        Args:
+            size (int): the size of the square
+        """
+        self.size = size
+
+    def area(self):
+        """class method that gets the area of square
+        Returns:
+            result
+        """
+
+        result = self.__size * self.__size
+        return (result)
+
+    @property
+    def size(self):
+        """class method that retrieve a private instance attribute"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
