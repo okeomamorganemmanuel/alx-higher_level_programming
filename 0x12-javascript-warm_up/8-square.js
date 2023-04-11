9fd84b2498080702e253846c041f8faed67cef2c
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-11 21:26:41 +0100

    add task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,12 @@
 const argx = process.argv.slice(2);
 
 if (isNaN(argx)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < argx; i++) {
-    let row = "";
+    let row = '';
     for (let j = 0; j < argx; j++) {
-      row += "X";
+      row += 'X';
     }
     console.log(row);
   }
