236633f5d937da968ec354c607bb9015c310b634
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-12 13:18:55 +0100

    hot fix task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
       return {};
     }
     this.width = w;
