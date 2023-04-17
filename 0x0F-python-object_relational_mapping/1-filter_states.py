e7d15ac737931c6c903a897e225780f95b049f88
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-17 04:52:45 +0100

    add task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,8 @@
     Usage: ./0-select_states.py <mysql username>\
  <mysql password> <database name>\
 """
-import MySQLdb, sys
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
