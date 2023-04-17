dc8ca78432fc2a5c8437f53c0c164488605d6add
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-17 20:22:04 +0100

    add task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-A script that displays all values in the states table of the database hbtn_0e_0_usa
-whose name matches that supplied as argument.
- Safe from SQL injections.
-Usage: ./3-my_safe_filter_states.py <mysql username> \
-                                     <mysql password> \
-                                     <database name> \
-                                     <state name searched>
+"""A script that takes in an argument that is safe from injection
+    attacks and displays all values in the states table of
+    hbtn_0e_0_usa where name matches the argument.
+    Usage: ./0-select_states.py <mysql username>\
+ <mysql password> <database name>\
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    search = sys.argv[4]
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s \
+                ORDER BY id ASC"
+    cursor.execute(query, (search,))
+    states = cursor.fetchall()
+
+    for state in states:
+        print(f"{state}")
