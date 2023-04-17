cdb7223bf36f0effdaa3c4f3f2c5a7c4ece60dc8
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-17 21:03:35 +0100

    add task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that prints the first  State objects from the database
+hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).first()
+    if states:
+        print("{}: {}".format(states.id, states.name))
+    else:
+        print(f"{Nothing}")
+
+    session.close()
